512e9f2206ca7d0adcb6d5ab8029711997fbdfcc
FernasG <fernandogoul7@gmail.com>
2023-11-18 20:43:31 -0300

    feat: add rabbitmq config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const RABBITMQ_URL = process.env.RABBITMQ_URL;
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [RABBITMQ_URL],
+      queue: 'notifications_queue',
+      queueOptions: { durable: true }
+    }
+  });
+
+  await app.listen();
 }
 bootstrap();
